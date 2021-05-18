765e3b7c8cc3504f960ec74702e1c34c4e8caf0f
ezbueno <ezandrobueno@yahoo.com.br>
2021-05-18 18:29:46 -0300

    Insert data for testing

--- bookstore-backend/src/main/java/com/ezandro/bookstore/services/DBService.java
+++ bookstore-backend/src/main/java/com/ezandro/bookstore/services/DBService.java
@@ -22,6 +22,7 @@ public class DBService {
 	public void instanciarBaseDados() {
 		Categoria cat1 = new Categoria(null, "Informática", "Livro de TI");
 		Categoria cat2 = new Categoria(null, "Ficção Científica", "Ficção Científica");
+		Categoria cat3 = new Categoria(null, "Biografias", "Livros de Biografia");
 
 		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Lorem Ipsum", cat1);
 		Livro l2 = new Livro(null, "Engenharia de Software", "Louis V. Gerstner", "Lorem Ipsum", cat1);
@@ -32,7 +33,7 @@ public class DBService {
 		cat1.getLivros().addAll(Arrays.asList(l1, l2));
 		cat2.getLivros().addAll(Arrays.asList(l3, l4, l5));
 
-		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		this.livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4, l5));
 
 	}
