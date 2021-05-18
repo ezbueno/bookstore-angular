3d3c996fd4be6e94c32b18d783f63694f91d4ac8
ezbueno <ezandrobueno@yahoo.com.br>
2021-05-18 17:11:26 -0300

    Test profile created

---
+++ bookstore-backend/src/main/java/com/ezandro/bookstore/services/DBService.java
@@ -0,0 +1,39 @@
+package com.ezandro.bookstore.services;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ezandro.bookstore.domain.Categoria;
+import com.ezandro.bookstore.domain.Livro;
+import com.ezandro.bookstore.repositories.CategoriaRepository;
+import com.ezandro.bookstore.repositories.LivroRepository;
+
+@Service
+public class DBService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private LivroRepository livroRepository;
+
+	public void instanciarBaseDados() {
+		Categoria cat1 = new Categoria(null, "Informática", "Livro de TI");
+		Categoria cat2 = new Categoria(null, "Ficção Científica", "Ficção Científica");
+
+		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Lorem Ipsum", cat1);
+		Livro l2 = new Livro(null, "Engenharia de Software", "Louis V. Gerstner", "Lorem Ipsum", cat1);
+		Livro l3 = new Livro(null, "The Time Machine", "H.G. Wells", "Lorem Ipsum", cat2);
+		Livro l4 = new Livro(null, "The War of the Worlds", "H.G. Wells", "Lorem Ipsum", cat2);
+		Livro l5 = new Livro(null, "I, Robot", "Isaac Asimov", "Lorem Ipsum", cat2);
+
+		cat1.getLivros().addAll(Arrays.asList(l1, l2));
+		cat2.getLivros().addAll(Arrays.asList(l3, l4, l5));
+
+		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		this.livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4, l5));
+
+	}
+}
