0b899a647b7d598d11fa768e85bd9a12c80ff939
ezbueno <ezandrobueno@yahoo.com.br>
2021-05-20 12:11:48 -0300

    PUT / update

--- bookstore-backend/src/main/java/com/ezandro/bookstore/services/CategoriaService.java
+++ bookstore-backend/src/main/java/com/ezandro/bookstore/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ezandro.bookstore.domain.Categoria;
+import com.ezandro.bookstore.dtos.CategoriaDTO;
 import com.ezandro.bookstore.repositories.CategoriaRepository;
 import com.ezandro.bookstore.services.exceptions.ObjectNotFoundException;
 
@@ -27,6 +28,13 @@ public class CategoriaService {
 
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
+		return categoriaRepository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDTO) {
+		Categoria obj = findById(id);
+		obj.setNome(objDTO.getNome());
+		obj.setDescricao(objDTO.getDescricao());
 		return categoriaRepository.save(obj);
 	}
 	
