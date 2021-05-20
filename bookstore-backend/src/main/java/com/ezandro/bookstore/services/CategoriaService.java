5f5b919c07772c44c6c046848b9ddef9d3522b40
ezbueno <ezandrobueno@yahoo.com.br>
2021-05-19 21:36:00 -0300

    Handling Exceptions

--- bookstore-backend/src/main/java/com/ezandro/bookstore/services/CategoriaService.java
+++ bookstore-backend/src/main/java/com/ezandro/bookstore/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.ezandro.bookstore.domain.Categoria;
 import com.ezandro.bookstore.repositories.CategoriaRepository;
+import com.ezandro.bookstore.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,6 @@ public class CategoriaService {
 	
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = categoriaRepository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id + ", Tipo: + " + Categoria.class.getName()));
 	}
 }
