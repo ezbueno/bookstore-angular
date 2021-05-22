a087ce98e3b92f6e0fa043d7e5554c04f8951083
ezbueno <ezandrobueno@yahoo.com.br>
2021-05-22 19:25:31 -0300

    categoria.service

--- bookstore-frontend/bookstore/src/app/components/views/categoria/categoria.service.ts
+++ bookstore-frontend/bookstore/src/app/components/views/categoria/categoria.service.ts
@@ -8,14 +8,12 @@ import { Categoria } from "./categoria.model";
   providedIn: "root",
 })
 export class CategoriaService {
-
   baseUrl: String = environment.baseUrl;
 
   constructor(private http: HttpClient) {}
 
-findAll(): Observable<Categoria[]> {
-  const url = `${this.baseUrl}/categorias`;
-  return this.http.get<Categoria[]>(`${url}`);
-}
-
+  findAll(): Observable<Categoria[]> {
+    const url = `${this.baseUrl}/categorias`;
+    return this.http.get<Categoria[]>(`${url}`);
+  }
 }
