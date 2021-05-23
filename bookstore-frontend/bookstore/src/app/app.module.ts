bc89ab65579795a65cef60a51e3b30455839f483
ezbueno <ezandrobueno@yahoo.com.br>
2021-05-23 15:01:10 -0300

    create method for category

--- bookstore-frontend/bookstore/src/app/app.module.ts
+++ bookstore-frontend/bookstore/src/app/app.module.ts
@@ -20,6 +20,12 @@ import { MatCardModule } from "@angular/material/card";
 import { MatTableModule } from "@angular/material/table";
 import { CategoriaReadComponent } from "./components/views/categoria/categoria-read/categoria-read.component";
 import { MatButtonModule } from "@angular/material/button";
+import { CategoriaCreateComponent } from "./components/views/categoria/categoria-create/categoria-create.component";
+
+import { FormsModule } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 
 @NgModule({
   declarations: [
@@ -29,6 +35,7 @@ import { MatButtonModule } from "@angular/material/button";
     NavComponent,
     HomeComponent,
     CategoriaReadComponent,
+    CategoriaCreateComponent,
   ],
   imports: [
     BrowserModule,
@@ -42,6 +49,10 @@ import { MatButtonModule } from "@angular/material/button";
     MatTableModule,
     HttpClientModule,
     MatButtonModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
