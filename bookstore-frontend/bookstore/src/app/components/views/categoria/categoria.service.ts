7aa8857a167a6036520dfc55e7b9d35e0db3569c
ezbueno <ezandrobueno@yahoo.com.br>
2021-05-24 19:24:13 -0300

    categoria-delete implemented

--- bookstore-frontend/bookstore/src/app/components/views/categoria/categoria.service.ts
+++ bookstore-frontend/bookstore/src/app/components/views/categoria/categoria.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
+import { Router } from "@angular/router";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 import { Categoria } from "./categoria.model";
@@ -18,9 +19,19 @@ export class CategoriaService {
     return this.http.get<Categoria[]>(url);
   }
 
+  findById(id: String): Observable<Categoria> {
+    const url = `${this.baseUrl}/categorias/${id}`;
+    return this.http.get<Categoria>(url);
+  }
+
   create(categoria: Categoria): Observable<Categoria> {
     const url = `${this.baseUrl}/categorias`;
     return this.http.post<Categoria>(url, categoria);
+  }
+
+  delete(id: String): Observable<void> {
+    const url = `${this.baseUrl}/categorias/${id}`;
+    return this.http.delete<void>(url);
   }
 
   message(msg: String): void {
